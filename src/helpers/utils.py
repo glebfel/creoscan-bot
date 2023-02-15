35b9915c8d4d4a4eb704fcff1c70f6f991fdd4fe
glebfel <felyust@list.ru>
2023-02-16 00:04:56 +0300

    fix bug in reels links resolving process

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,18 +1,19 @@
 from helpers.base import BaseHelper
-from helpers.instagram import InstagramSelectedUserStoryParserHelper, InstagramUserStoriesParserHelper
+from helpers.instagram import InstagramSelectedUserStoryParserHelper, InstagramUserStoriesParserHelper, \
+    InstagramSelectedReelParserHelper
 
 
 def get_helper_class_from_link(text: str) -> BaseHelper:
     if '/stories/' in text:
         return InstagramSelectedUserStoryParserHelper
-    elif '/reels/' in text:
-        pass
+    elif '/reel/' in text:
+        return InstagramSelectedReelParserHelper
     else:
         return InstagramUserStoriesParserHelper
 
 
 def extract_username_from_link(link: str) -> str:
-    if '/reels/' in link:
+    if '/reel/' in link:
         return link
     if '/stories/' in link:
         return link.strip().strip('/').split('/')[-2]
