2d22e15506a0b0008004d852ad784621df7e611e
glebfel <felyust@list.ru>
2023-03-21 16:17:37 +0300

    add handlers helper

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,4 +1,6 @@
+from common.models import ThirdPartyAPISource
 from helpers.base import BaseHelper
+from helpers.clients import TikTokRapidAPIClient, InstagramRapidAPIClient
 from helpers.instagram import (InstagramSelectedUserStoryParserHelper,
                                InstagramUserStoriesParserHelper,
                                InstagramSelectedReelParserHelper,
@@ -8,6 +10,7 @@ from helpers.instagram import (InstagramSelectedUserStoryParserHelper,
 from helpers.tiktok import (TikTokSelectedMusicParserHelper,
                             TikTokSelectedVideoParserHelper,
                             TikTokUnknownMediaTypeParserHelper)
+from models import BotModule
 
 
 def get_helper_class_from_link_instagram(text: str) -> BaseHelper:
@@ -33,6 +36,19 @@ def get_helper_class_from_link_tiktok(text: str) -> BaseHelper:
     else:
         # for universal links: https://vt.tiktok.com/ZS8rh8oAH/
         return TikTokUnknownMediaTypeParserHelper
+
+
+def get_monitoring_handler(module: BotModule, social_network: str, media_type: str) -> callable:
+    if social_network == ThirdPartyAPISource.instagram.value:
+        match media_type:
+            case module.reels_button:
+                return InstagramRapidAPIClient().get_instagram_reels_by_username
+            case module.posts_button:
+                return InstagramRapidAPIClient().get_instagram_posts_by_username
+            case module.stories_button:
+                return InstagramRapidAPIClient().get_instagram_user_stories
+    else:
+        return TikTokRapidAPIClient().get_tiktok_user_video_by_username
 
 
 def extract_username_from_link_instagram(link: str) -> str:
