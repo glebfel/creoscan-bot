434525dae274ba6f88fb93dc054f3fc8a64d59c2
glebfel <felyust@list.ru>
2023-03-22 13:46:53 +0300

    optimize and clean code

--- src/helpers/utils.py
+++ src/helpers/utils.py
@@ -1,12 +1,10 @@
 import validators
-
 from pyrogram.types import (
     Message,
 )
-from common.models import ThirdPartyAPISource
+
 from exceptions import WrongInputException
 from helpers.base import BaseHelper
-from helpers.clients import TikTokRapidAPIClient, InstagramRapidAPIClient
 from helpers.instagram import (InstagramSelectedUserStoryParserHelper,
                                InstagramUserStoriesParserHelper,
                                InstagramSelectedReelParserHelper,
@@ -16,7 +14,6 @@ from helpers.instagram import (InstagramSelectedUserStoryParserHelper,
 from helpers.tiktok import (TikTokSelectedMusicParserHelper,
                             TikTokSelectedVideoParserHelper,
                             TikTokUnknownMediaTypeParserHelper)
-from models import BotModule
 
 
 def get_helper_class_from_link_instagram(text: str) -> BaseHelper:
@@ -42,19 +39,6 @@ def get_helper_class_from_link_tiktok(text: str) -> BaseHelper:
     else:
         # for universal links: https://vt.tiktok.com/ZS8rh8oAH/
         return TikTokUnknownMediaTypeParserHelper
-
-
-def get_monitoring_handler(module: BotModule, social_network: str, media_type: str) -> callable:
-    if social_network == ThirdPartyAPISource.instagram.value:
-        match media_type:
-            case module.reels_button:
-                return InstagramRapidAPIClient().get_instagram_reels_by_username
-            case module.posts_button:
-                return InstagramRapidAPIClient().get_instagram_posts_by_username
-            case module.stories_button:
-                return InstagramRapidAPIClient().get_instagram_user_stories
-    else:
-        return TikTokRapidAPIClient().get_tiktok_user_videos_by_username
 
 
 def extract_username_from_link(message: Message) -> str:
