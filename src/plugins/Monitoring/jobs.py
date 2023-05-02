ec8ec9f6bf551da185e4b7be5fac30956283d766
glebfel <felyust@list.ru>
2023-05-03 02:07:47 +0300

    fix monitoring job

--- src/plugins/Monitoring/jobs.py
+++ src/plugins/Monitoring/jobs.py
@@ -34,27 +34,24 @@ async def start_monitoring(
 ) -> None:
     custom_error_message: str = getattr(module, 'error_text', api_adapter_module.unhandled_error_text)
     try:
-        # get last media from api source
-        new_data = await get_monitoring_media_handler_func(module=module, social_network=social_network,
-                                                           media_type=media_type)(
-            nickname, limit=1)
-        if not new_data.items:
-            return
-        else:
-            new_data = new_data.items[0]
         # get job start time
         monitoring_start_date = (
             await UserMonitoringRequestsDBConnector.get_last_user_monitoring(user_id=message.chat.id)).start_date
-        # compare last monitoring data from storage
-        last_monitoring_data = await UserMonitoringRequestsDBConnector.get_last_updated_monitoring_data(message.chat.id)
-        if ((not last_monitoring_data and monitoring_start_date < new_data.taken_at) or
-                (last_monitoring_data and last_monitoring_data.taken_at < new_data.taken_at)):
+        # get last monitoring media date from storage
+        last_monitoring_media_date = await UserMonitoringRequestsDBConnector.get_last_monitoring_media_date(
+            message.chat.id)
+        # get last media from api source
+        new_data = await get_monitoring_media_handler_func(module=module, social_network=social_network,
+                                                           media_type=media_type)(nickname,
+                                                                                  start_from=last_monitoring_media_date
+                                                                                  if last_monitoring_media_date else monitoring_start_date)
+        if new_data.items:
             result_message = module.result_text.format(media_type=media_type, nickname=nickname)
             await message_handler(chat_id=message.chat.id, message=result_message, media=new_data)
-        # save last item id to storage
-        await UserMonitoringRequestsDBConnector.save_last_updated_monitoring_data(data=new_data,
-                                                                                  user_id=message.chat.id)
-
+            # save last monitoring media date to storage
+            await UserMonitoringRequestsDBConnector.save_last_monitoring_media_data(
+                date=new_data.items[-1].taken_at,
+                user_id=message.chat.id)
     except exceptions.AccountIsPrivate:
         await message_handler(chat_id=message.chat.id, message=api_adapter_module.error_text_account_private)
     except exceptions.AccountNotExist:
